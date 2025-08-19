f4fb0c3a811ff4392b7ad985a0f0ccb33ae0ea17
yufe-xu-scacap <yufei.xu@scalable.capital>
2025-08-19 10:20:44 +0200

    Update windows_run.bat

--- windows_run.bat
+++ windows_run.bat
@@ -3,21 +3,17 @@ echo =============================================
 echo  Setting up Streamlit Environment...
 echo =============================================
 
-:: Check if the virtual environment folder exists, if not, create it
-IF NOT EXIST venv (
-    echo Creating virtual environment...
-    python -m venv venv
-)
+:: Use the 'py' command to be more reliable on Windows
+:: The '-3' flag specifically looks for a Python 3 installation
+py -3 -m venv venv
 
-:: Activate the virtual environment and install dependencies
 echo Activating virtual environment and installing packages...
 call venv\Scripts\activate
 pip install -r requirements.txt
 
-:: Run the Streamlit app
 echo =============================================
 echo  Starting the Streamlit App...
 echo =============================================
-streamlit run Main_Page.py
+streamlit run app.py
 
 pause
